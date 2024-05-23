9caed92c1a53057eed0e1926e988a76125c83932
samuel <samuelrey010@gmail.com>
2024-05-23 16:57:14 -0700

    create initial state and outline handlers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import React, { useState } from 'react';
 import logo from "./logo.svg";
+import sample from "./sample.json";
 import "./App.css";
 
 function App() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
+    
+    const handleChange = (e) => {
+        setSearchQuery(e.target.value)
+        console.log(searchQuery)
+    }
+
+    const handleSubmit = (e) => {
+        console.log(searchQuery)
+    }
+
+    if (loading) {
+        return <h2>Loading...</h2>
+    }
+    if (error) {
+        return <h2>Error</h2>
+    }
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    <input placeholder="Search for an image" onChange={handleChange}/>
+                    <button onClick={handleSubmit}>Submit</button>
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <img className="App-link" src={sample.link} alt="silly-otter" />
             </header>
         </div>
     );
