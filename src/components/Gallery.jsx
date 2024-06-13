371f9083d72beddfcfdb85ebe18a50befc5bebd6
Sammy <samuelrey010@gmail.com>
2024-06-12 23:58:50 -0700

    extract helper, useEffect

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,39 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import GalleryImage from "./GalleryImage";
 import Modal from "./Modal";
 import useModal from "../hooks/useModal";
 
+const getTagsSortedByOccurence = (images) => {
+    const tagByCount = new Map();
+
+    images.forEach(({ tags }) => {
+        tags.forEach(({ display_name }) => {
+            if (tagByCount.has(display_name)) {
+                tagByCount.set(display_name, tagByCount.get(display_name) + 1);
+            } else {
+                tagByCount.set(display_name, 1);
+            }
+        });
+    });
+
+    return [...tagByCount.entries()].sort((a, b) => b[1] - a[1]);
+};
+
 const Gallery = ({ images }) => {
     const [selectedImage, setSelectedImage] = useState(null);
+    const [tags, setTags] = useState([]);
     const { isOpen, openModal, closeModal } = useModal();
+
+    useEffect(() => {
+        setTags(getTagsSortedByOccurence(images));
+    }, [images]);
 
     const handleImageClick = (image) => {
         setSelectedImage(image);
         openModal();
     };
-
-    const aggregateTags = (images) => {
-        const tagByCount = new Map();
-
-        images.forEach(({ tags }) => {
-            tags.forEach(({ display_name }) => {
-                if (tagByCount.has(display_name)) {
-                    tagByCount.set(
-                        display_name,
-                        tagByCount.get(display_name) + 1
-                    );
-                } else {
-                    tagByCount.set(display_name, 1);
-                }
-            });
-        });
-
-        const sortedByCount = Array.from(tagByCount);
-        sortedByCount.sort((a, b) => b[1] - a[1]);
-        return sortedByCount;
-    };
-
-    const uniqueTags = aggregateTags(images);
 
     return (
         <div data-testid="gallery">
